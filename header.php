fff5e3ca0fc9897d40b4b403bf1eeb136601b35a
EsKepok <eskepok@gmail.com>
2025-02-06 00:32:38 +0700

    1

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Es Kepok</title>
+   </head>
+   <body>
+      <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+         <header class="masthead mb-auto">
+            <div class="inner">
+               <h3 class="masthead-brand" href="index.html">Es Kepok</h3>
+               <nav class="nav nav-masthead justify-content-center">
+                  <a href="index.html" class="nav-link active">Home</a>
+                  <a href="#" class="nav-link">About</a>
+                  <a href="#" class="nav-link">Project</a>
+               </nav>
+            </div>
+         </header>
+
+
+         <footer class="mastfoot mt-auto">
+            <div class="inner">
+               <p>
+                  Es Kepok | &copy; Copyright reserved | <a href="mailto:eskepok@gmail.com">eskepok@gmail.com</a>
+               </p>
+            </div>
+         </footer>
+      </div>
+   </body>
+</html>
